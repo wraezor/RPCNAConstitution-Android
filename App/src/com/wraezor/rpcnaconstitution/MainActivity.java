6df79c5c7401fe8b130d32fe15dfa48a7335b60c
Jordan Dohms <wraezor@gmail.com>
2014-05-21 13:13:41 -0600

    Preliminary work on navigation drawer.

--- App/src/com/wraezor/rpcnaconstitution/MainActivity.java
+++ App/src/com/wraezor/rpcnaconstitution/MainActivity.java
@@ -1,7 +1,30 @@
 package com.wraezor.rpcnaconstitution;
 
 import android.app.Activity;
+import android.os.Bundle;
+//import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+    private String[] mMenuStringItems;
+    //private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+        
+        mMenuStringItems = getResources().getStringArray(R.array.RPC_menu_items);
+        //mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.menu_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.menu_item, mMenuStringItems));
+        // Set the list's click listener
+        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+    }
 
 }
